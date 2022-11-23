fec54fbd5f746f39e78de24a6493289b6611756e
sunaina <sharmasunaina053@gmail.com>
2022-11-23 23:17:43 +0530

    second comment

--- fifth.java
+++ fifth.java
@@ -1,11 +1,19 @@
+// Enter any integer from 1-3 and print in word
+//if the given value is beyond the range than it gives msg out of range
 import java.util.Scanner;
-class fifth {
-    public static void main(String[]args)
-    {
-        Scanner obj= new Scanner(System.in);
-        System.out.println("enter the sales");
-        int sales=obj.nextInt();
-        if()
+class fifth{
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        System.out.println("Enter any integer from 1-3");
+        int a=obj.nextInt();
+        if (a==1)
+        System.out.println(+a+" =one");
+       else if (a==2)
+        System.out.println(+a+"=two");
+       else if (a==3)
+        System.out.println(+a+"three");
+        else
+        System.out.println("wrong entered");
 
     }
 }
